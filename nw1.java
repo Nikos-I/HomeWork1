c3e50d2f1a55bfe06f7ba03715e0987b16c79559
Nikos I <nik.norht@gmail.com>
2022-11-24 14:18:22 +0300

    Seminar1 ДЗ1 Вычислить треугольное число... готово

--- nw1.java
+++ nw1.java
@@ -7,21 +7,23 @@ import com.google.common.math.BigIntegerMath;
 public class nw1 {
     public static void main(String[] args) {
         int numVal = 0;
-        while (numVal<=0) {
-            System.out.print( "Введите целое число: ");    
+        while (numVal <= 0) {
+            System.out.print("Введите целое число: ");
             Scanner in = new Scanner(System.in);
             try {
                 numVal = in.nextInt();
             } catch (Exception e) {
                 numVal = 0;
             }
-            if (numVal<=0) System.out.println( "Неверное значение, введите заново");    
-        } 
-
-        int numTriangular = numVal*(numVal+1)/2;
+            if (numVal <= 0)
+                System.out.println("Неверное значение, введите заново");
+            in.close();
+        }
+        
+        int numTriangular = numVal * (numVal + 1) / 2;
         BigInteger bigIntFactorial = BigInteger.ONE;
         bigIntFactorial = BigIntegerMath.factorial(numVal);
-        System.out.printf("Треугольное число для %d: %d \n", numVal, numTriangular);    
-        System.out.printf("Факториал для %d: %s \n", numVal, bigIntFactorial.toString().replaceAll("0+$", ""));    
+        System.out.printf("Треугольное число для %d: %d \n", numVal, numTriangular);
+        System.out.printf("Факториал для %d: %s \n", numVal, bigIntFactorial.toString().replaceAll("0+$", ""));
     }
-}    
+}
